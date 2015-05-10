c5a739f9c5d8695f08d799d4411b104b11a911f1
Bernardo Martínez Garrido <programming@wandrell.com>
2015-05-10 12:56:10 +0200

    Removed unused repositories.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/punkabuilder/repository/DesktopMutationRepository.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/punkabuilder/repository/DesktopMutationRepository.java
@@ -1,9 +1,5 @@
 package com.wandrell.tabletop.punkapocalyptic.punkabuilder.repository;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Component;
 
 import com.wandrell.pattern.repository.DefaultQueryData;
@@ -17,28 +13,6 @@ public final class DesktopMutationRepository extends JPARepository<Mutation>
 
     public DesktopMutationRepository() {
         super(new DefaultQueryData("SELECT mutation FROM Mutation mutation"));
-    }
-
-    @Override
-    public final Collection<Mutation> getByNamesList(
-            final Collection<String> names) {
-        final Map<String, Object> params;
-        final StringBuilder namesResult;
-
-        namesResult = new StringBuilder();
-        for (final String name : names) {
-            if (namesResult.length() > 0) {
-                namesResult.append(", ");
-            }
-            namesResult.append(name);
-        }
-
-        params = new LinkedHashMap<>();
-        params.put("mutations", namesResult.toString());
-
-        return getCollection(new DefaultQueryData(
-                "SELECT mutation FROM Mutation mutation WHERE mutation.name = :mutations",
-                params));
     }
 
 }
